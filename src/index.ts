d172d4be8351064be014eb1fab19fcd4508e8410
GoDotDotDot <chukui0202@gmail.com>
2024-04-24 15:03:18 +0800

    feat: update env load

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env tsx
 // install local package: pnpm install -g "deworker-cli@link:$(pwd)"
 import { Command } from 'commander';
+import dotenv from 'dotenv';
 
 import workerCMD from './command/worker/index.js';
 import peerCMD from './command/peer/index.js';
 import configCMD from './command/config/index.js';
+
+dotenv.config();
 
 const program = new Command('deworker');
 
